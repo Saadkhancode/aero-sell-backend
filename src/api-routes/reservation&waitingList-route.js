50af035046ed3a129d7e64cedb53b5604b1e5adb
Luqmanyousafzai <luqman.dagai@gmail.com>
2023-02-10 03:01:05 -0800

    api change

--- src/api-routes/reservation&waitingList-route.js
+++ src/api-routes/reservation&waitingList-route.js
@@ -3,6 +3,7 @@ const router=express.Router();
 import {
 getReservedTables,
 getWaitingTables,
+getReservationAndWaitingListbyId,
 postReservationAndWaitingList,
 updateReservationAndWaitingList,
 deleteReservationAndWaitingList
@@ -10,6 +11,7 @@ deleteReservationAndWaitingList
 router.get('/reserved',getReservedTables)
 router.get('/waiting',getWaitingTables)
 router.post('/table-operator',postReservationAndWaitingList)
+router.get('/table-operator/:_id',getReservationAndWaitingListbyId)
 router.put('/table-operator/:_id',updateReservationAndWaitingList)
 router.delete('/table-operator/:_id',deleteReservationAndWaitingList)
 export default router;
