c5a4877e31636608649f2a00c863a787dfe21bde
Saadkhancode <saadk6401@gmail.com>
2023-02-28 16:33:29 +0500

    employee login api changes done

--- src/api/user.js
+++ src/api/user.js
@@ -1,35 +1,41 @@
 import { superUser, User } from '../models/User.js'
+import employee from '../models/employee.js'
 import jwt from "jsonwebtoken";
 
 
 export const getUser = async (req, res) => {
-    const user = await User.find(req)
-    res.send(user)
+  const user = await User.find(req)
+  res.send(user)
 }
 export const getUserById = async (req, res) => {
-    const user = await User.find(req.params)
-    res.send(user)
+  const user = await User.find(req.params)
+  res.send(user)
 }
 export const getSuperUser = async (req, res) => {
-    const user = await superUser.find(req)
-    res.send(user)
+  const user = await superUser.find(req)
+  res.send(user)
 }
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
-  const user = await User.findOne({ email }) || await superUser.findOne({ email })
+  const user = await User.findOne({ email }) || await superUser.findOne({ email }) || await employee.findOne({ email })
   if (!user) {
     return res.status(400).send({ message: "User not found" });
   }
   if (user.password !== password) {
     return res.status(400).send({ message: "Wrong password" });
   }
-  const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
-  const userId = { _id: user._id }
-  const role=user.role;
-  const loginDate=user.createdDate
-  console.log('role: ', role);
-  res.send({ message: "user login successfully", token, userId,role,loginDate,email:user.email });
+  console.log('role: ', user.role);
+  if (user.role == 'admin' || user.role == 'superadmin') {
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+    const userId = { _id: user._id }
+    const role = user.role;
+    const loginDate = user.createdDate
+    return res.send({ message: "user login successfully", token, userId, role, loginDate, email: user.email });
+  } else if (user.role == 'employee') {
+    const userId = { _id: user.userId }
+    return res.status(200).send({ message: "Employee Login Successfully", userId, startDate: user.startDate, employeId: user._id, firstName: user.firstName, lastName: user.lastName, role: user.role });
+  }
 
 }
 
@@ -38,7 +44,7 @@ export const updateUser = async (req, res) => {
   let data = await User.findByIdAndUpdate(
     { _id: req.params._id }, {
     $set: req.body
-  },{}, { new: true }
+  }, {}, { new: true }
   );
   if (data) {
     res.send({ message: "User data updated successfully" });
