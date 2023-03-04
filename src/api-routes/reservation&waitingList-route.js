d1764c57b555d5946f146c8e472cad53034a3956
Saadkhancode <saadk6401@gmail.com>
2023-03-04 16:51:26 +0800

    resrevation getby id api added

--- src/api-routes/reservation&waitingList-route.js
+++ src/api-routes/reservation&waitingList-route.js
@@ -5,10 +5,12 @@ getReservedTables,
 getWaitingTables,
 postReservationAndWaitingList,
 updateReservationAndWaitingList,
-deleteReservationAndWaitingList
+deleteReservationAndWaitingList,
+getReservationAndWaitingListById
 } from '../api/tables-reservation&waitingList.js'
 router.get('/reserved',getReservedTables)
 router.get('/waiting',getWaitingTables)
+router.get('/table-operator/:_id',getReservationAndWaitingListById)
 router.post('/table-operator',postReservationAndWaitingList)
 router.put('/table-operator/:_id',updateReservationAndWaitingList)
 router.delete('/table-operator/:_id',deleteReservationAndWaitingList)
