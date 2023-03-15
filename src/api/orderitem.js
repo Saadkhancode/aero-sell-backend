28cc56e8880db82628dd1907ae34c173caf01592
Saadkhancode <saadk6401@gmail.com>
2023-03-15 11:15:00 +0500

    email send for low stock with order

--- src/api/orderitem.js
+++ src/api/orderitem.js
@@ -1,6 +1,7 @@
 import orderitem from '../models/orderitem.js'
 import customer from '../models/customer.js'
 import Product from '../models/product.js'
+import sendMail from '../middlewares/send-email.js'
 
 export const getOrderItemByUserId = async (req, res) => {
     let filter = {}
@@ -20,27 +21,29 @@ export const getOrderItemById = async (req, res) => {
 }
 
 export const postOrderItem = async (req, res) => {
-    const { orderId,table, product, selectedModifiers,points, taxValue, productWithQty, priceExclTax, lineValueExclTax, lineValueTax, lineValue, units, text,customerId,dueamount,createdAt,updatedAt, userId,paymentType } = req.body;
-    const data = await new orderitem({ orderId,table, product,selectedModifiers, points, taxValue, productWithQty, priceExclTax, lineValueExclTax, lineValueTax, lineValue, units, text,customerId, dueamount,createdAt,updatedAt, userId,paymentType });
+    const { orderId, table, product, selectedModifiers, points, taxValue, productWithQty, priceExclTax, lineValueExclTax, lineValueTax, lineValue, units, text, customerId, dueamount, createdAt, updatedAt, userId, paymentType } = req.body;
+    const data = await new orderitem({ orderId, table, product, selectedModifiers, points, taxValue, productWithQty, priceExclTax, lineValueExclTax, lineValueTax, lineValue, units, text, customerId, dueamount, createdAt, updatedAt, userId, paymentType });
     await data.save().then(async (result) => {
-        const customerPoints =priceExclTax / 5;
-        console.log("customerpoints:",customerPoints);
-        const customerById=await customer.findById(customerId)
-        console.log("customerBeforeAddedPoints",customerById);
-        // const selectedProds=result.product?.filter((item=> item._id) )
-        // const filter=Object.assign({},selectedProds)
-        // console.log('filter: ', filter);
-        // const productsById=await Product.find().populate('product')
-        // console.log('productById: ', productsById);
-        
-        if(customerById){
-            const  customerdata=  await customer.findByIdAndUpdate(customerById, { $set: { "CustomerLoyalty.Points": customerById.CustomerLoyalty.Points + customerPoints } })
-            console.log("customerAfterAddedPoints",customerdata);
+        const customerPoints = priceExclTax / 5;
+        const customerById = await customer.findById(customerId)
+        const products = await Product.find({userId}).populate('userId')
+        let filteredProductsName = []
+        let userEmail = products[0].userId.email
+        products?.filter((item) => {
+            if (item.totalQuantity <= 5) {
+                filteredProductsName.push(item.name)
+            }
+        })
+        console.log('filterProduct: ', filteredProductsName);
+        sendMail(userEmail, "Low Stock Alerts", `<h2 style="background-color: #f1f1f1; padding: 20px;width:50%">These Products Are  Low  In  Stock</h2><br><h3 style="background-color: #f1f1f1; width:60%">${filteredProductsName}</h3>`)
+        if (customerById) {
+            const customerdata = await customer.findByIdAndUpdate(customerById, { $set: { "CustomerLoyalty.Points": customerById.CustomerLoyalty.Points + customerPoints } })
+            console.log("customerAfterAddedPoints", customerdata);
             res.json({
                 orderId: result.orderId,
                 product: result.product,
-                table:result.table,
-                selectedModifiers:result.selectedModifiers,
+                table: result.table,
+                selectedModifiers: result.selectedModifiers,
                 dueamount: result.dueamount,
                 points: result.points,
                 taxValue: result.taxValue,
@@ -54,11 +57,11 @@ export const postOrderItem = async (req, res) => {
                 text: result.text,
                 userId: result.userId,
                 customerId: result.customerId,
-                paymentType:result.paymentType,
-                createdAt:result.createdAt,
-                updatedAt:result.updatedAt
+                paymentType: result.paymentType,
+                createdAt: result.createdAt,
+                updatedAt: result.updatedAt
             })
-        }else{
+        } else {
             res.json({
                 orderId: result.orderId,
                 product: result.product,
@@ -75,12 +78,12 @@ export const postOrderItem = async (req, res) => {
                 text: result.text,
                 userId: result.userId,
                 customerId: result.customerId,
-                paymentType:result.paymentType,
-                createdAt:result.createdAt,
-                updatedAt:result.updatedAt
+                paymentType: result.paymentType,
+                createdAt: result.createdAt,
+                updatedAt: result.updatedAt
             })
         }
-    
+
     }).catch(err => {
         res.status(400).send('unable to save database');
         console.log(err)
@@ -95,18 +98,18 @@ export const updateOrderItem = async (req, res) => {
     },
         { new: true }
     );
-    if (data) 
+    if (data)
         res.send({ message: "orderitem data updated successfully" });
-    else 
+    else
         res.send({ message: "orderitem data cannot be updated successfully" })
 }
 
 export const deleteOrderItem = async (req, res) => {
     console.log(req.params)
     let data = await orderitem.deleteOne(req.params)
-    if (data) 
+    if (data)
         res.send({ message: "orderitem data delete successfully" });
-    else 
+    else
         res.send({ message: "orderitem data cannot delete successfully" })
 }
 
