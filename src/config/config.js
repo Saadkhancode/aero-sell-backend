30d3f1decaed232ac63cbb58a6f654266b70771c
Saadkhancode <saadk6401@gmail.com>
2023-04-14 20:07:44 +0500

    backend crash issue

--- src/config/config.js
+++ src/config/config.js
@@ -3,38 +3,8 @@ import mongoose from "mongoose"
 import dotenv from 'dotenv'
 
 dotenv.config();
-if (process.env.NODE_ENV === 'production') {
-  
-  mongoose.connect(process.env.PROD_MONGODB_URI, {
-      dbName: process.env.PROD_DB_NAME,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => {
-      console.log('mongodb connected.')
-    })
-    .catch((err) => console.log(err.message))
-  
-  mongoose.connection.on('connected', () => {
-    console.log('Mongoose connected to db')
-  })
-  
-  mongoose.connection.on('error', (err) => {
-    console.log(err.message)
-  })
-  
-  mongoose.connection.on('disconnected', () => {
-    console.log('Mongoose connection is disconnected.')
-  })
-  
-  process.on('SIGINT', async () => {
-    await mongoose.connection.close()
-    process.exit(0)
-  })
-  
-} else if (process.env.NODE_ENV === 'development') {
-  mongoose.connect(process.env.DEV_MONGODB_URI, {
-    dbName: process.env.DEV_DB_NAME,
+mongoose.connect(process.env.MONGODB_URI, {
+    dbName: process.env.DB_NAME,
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -58,6 +28,36 @@ mongoose.connection.on('disconnected', () => {
 process.on('SIGINT', async () => {
   await mongoose.connection.close()
   process.exit(0)
-});
-}
+})
+// if (process.env.NODE_ENV === 'production') {
+  
+  
+// } else if (process.env.NODE_ENV === 'development') {
+//   mongoose.connect(process.env.DEV_MONGODB_URI, {
+//     dbName: process.env.DEV_DB_NAME,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log('mongodb connected.')
+//   })
+//   .catch((err) => console.log(err.message))
+
+// mongoose.connection.on('connected', () => {
+//   console.log('Mongoose connected to db')
+// })
+
+// mongoose.connection.on('error', (err) => {
+//   console.log(err.message)
+// })
+
+// mongoose.connection.on('disconnected', () => {
+//   console.log('Mongoose connection is disconnected.')
+// })
+
+// process.on('SIGINT', async () => {
+//   await mongoose.connection.close()
+//   process.exit(0)
+// });
+// }
 
