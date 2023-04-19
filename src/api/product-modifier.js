64f37ad8cd13b6ee15d00ac69ea1bb6d8132c4a5
Ihtizaz Ahmad <ihtizazahmad2002@gmail.com>
2023-04-19 15:51:30 +0500

    add modifires

--- src/api/product-modifier.js
+++ src/api/product-modifier.js
@@ -1,48 +1,88 @@
-import productModifierModel from "../models/productmodifier.js";
-export const getProductModifier=async(req,res)=>{
-    let filter={}
-    if(req.query.userId){
-        filter={userId:req.query.userId.split(',')}
-    }else if (req.query.productId){
-        filter={productId:req.query.productId.split(',')}
+import { Category1, productModifierModel } from "../models/productmodifier.js";
+export const getProductModifier = async (req, res) => {
+    let filter = {}
+    if (req.query.userId) {
+        filter = { userId: req.query.userId.split(',') }
+    } else if (req.query.productId) {
+        filter = { productId: req.query.productId.split(',') }
     }
-    let modifierData=await productModifierModel.find(filter).populate("Size").populate("productId",'name')
+    let modifierData = await productModifierModel.find(filter).populate("Size").populate("productId", 'name')
     res.send(modifierData)
 }
-export const postProductModifier=async(req,res)=>{
-     const {Size,Caffein,Espresso,Flavors,userId,productId}=req.body
-      const modifier=new productModifierModel({Size,Caffein,Espresso,Flavors,userId,productId})
-      await modifier.save().then(results=>{
+export const postProductModifier = async (req, res) => {
+    const { Size, Caffein, Espresso, Flavors, userId, productId } = req.body
+    const modifier = new productModifierModel({ Size, Caffein, Espresso, Flavors, userId, productId })
+    await modifier.save().then(results => {
         console.log("modifier data send to database")
         res.json({
-            Size:results.Size,
-            Caffein:results.Caffein,
-            Espresso:results.Espresso,
-            Flavors:results.Flavors,
-            userId:results.userId,
-            productId:results.productId
+            Size: results.Size,
+            Caffein: results.Caffein,
+            Espresso: results.Espresso,
+            Flavors: results.Flavors,
+            userId: results.userId,
+            productId: results.productId
         })
-      }).catch(err=>{
+    }).catch(err => {
         res.status(400).send('unable to save modifier data to database')
         console.log(err);
-      })
+    })
 }
-export const updateProductModifier=async(req,res)=>{
-    const modifier=await productModifierModel.findByIdAndUpdate(
-        {_id:req.params._id},{
-            $set:req.body
-        },{new:true})
-        if(modifier){
-            res.status(200).send("modifier data updated successfully")
-        }else{
-            res.status(400).send("modifier data cannot be updated")
-        }
+export const updateProductModifier = async (req, res) => {
+    const modifier = await productModifierModel.findByIdAndUpdate(
+        { _id: req.params._id }, {
+        $set: req.body
+    }, { new: true })
+    if (modifier) {
+        res.status(200).send("modifier data updated successfully")
+    } else {
+        res.status(400).send("modifier data cannot be updated")
+    }
 }
-export const deleteModifier=async(req,res)=>{
-    const modifier=await productModifierModel.findByIdAndDelete({_id:req.params._id})
-    if(modifier){
+export const deleteModifier = async (req, res) => {
+    const modifier = await productModifierModel.findByIdAndDelete({ _id: req.params._id })
+    if (modifier) {
         res.status(200).send("modifier data delete successfully")
-    }else{
+    } else {
         res.status(400).send("modifier data cannot be deleted")
     }
 }
+
+
+
+//get
+export const getaddnewcategory = async (req, res) => {
+    const categories = await Category1.find();
+    res.json(categories);
+};
+
+//post
+export const addnewcategory = async (req, res) => {
+    const { categories, subcategories, Size, productId, userId } = req.body;
+
+    const newCategory = await new Category1({ categories: req.body.categories, Size: req.body.Size, productId: req.body.productId, userId: req.body.userId });
+    const savedCategory = await newCategory.save();
+    res.json(savedCategory);
+};
+
+//put 
+
+export const putaddnewcategory = async (req, res) => {
+    const { id } = req.params;
+    const { name, subcategories } = req.body;
+
+    const updatedCategory = await Category1.findOneAndUpdate(
+        { _id: id },
+        { name, subcategories },
+        { new: true }
+    );
+
+    res.json(updatedCategory);
+};
+
+//delete
+
+export const dleteaddnewcategory = async (req, res) => {
+    const { id } = req.params;
+    await Category1.deleteOne({ _id: id });
+    res.sendStatus(204);
+};
