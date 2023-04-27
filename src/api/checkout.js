e188bc69e2d3ad42457c7e382fb796b97882db98
Saadkhancode <saadk6401@gmail.com>
2023-04-27 15:43:04 +0500

    stripe charge apiu created

--- src/api/checkout.js
+++ src/api/checkout.js
@@ -2,32 +2,25 @@ import Stripe from 'stripe';
 let stripe = Stripe('sk_test_51MiZTVF1YkHoz4Y5AsHfg9ovHa5zsRFHCfVrHSy5XKvxKtdKSMHpzQ5V0wEfcGHVfoEQ50NjXhCP0aF2aC1Mc05300eCAJlRxu');
 export const Checkout = async (req, res) => {
   try {
-    const { amount, source, connectedAccountId,application_fee_amount } = req.body;
-  
+    const { amount,currency, source,  application_fee_amount, transfer_data, description } = req.body.stripeToken;
+
+    console.log("req body stripe charge", req.body);
     // Create the charge with the connected account ID and application fee
     const charge = await stripe.charges.create({
-      "amount": amount,
-      "currency": "usd",
-      "source": source,
-      // "description": "Example charge",
-      "application_fee_amount": application_fee_amount,
-      "transfer_data": {
-        "destination": connectedAccountId
-      }
+     amount,
+      currency,
+      source,
+      description,
+      application_fee_amount,
+      transfer_data
+      // "transfer_data": {
+      //   "destination": connectedAccountId
+      // }
     });
-    // const charge = await stripe.charges.create({
-    //     amount,
-    //     currency: 'usd',
-    //     source,
-    //     application_fee_amount: amount * 100, // Replace with your desired application fee percentage
-    //     transfer_data: {
-    //       destination: connectedAccountId,
-    //     },
-    //   });
-  
-      res.json({ message: 'Charge successfully created!', charge });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ message: 'Something went wrong', error });
-    }
+
+    res.json({ message: 'Charge successfully created!', charge });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Something went wrong', error });
+  }
 };
