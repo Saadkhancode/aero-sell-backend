7cde31e3c3ed07ecb92a5117293008613ce954b0
Saadkhancode <saadk6401@gmail.com>
2023-05-31 20:27:24 +0500

    chnages to stripe app subscripton

--- src/index.js
+++ src/index.js
@@ -48,10 +48,11 @@ const __filename=fileURLToPath(import.meta.url)
 const __dirname=path.dirname(__filename)
 //middelwares
 app.use(cors({
-    origin: 'https://patronworks.com',
+    origin: true,
     credentials: true,
+    defaultErrorHandler: false,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  }));
+}));
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.json());
