4cb9adafb5d94aa23ae68e85c0a347eddf09e124
Saadkhancode <saadk6401@gmail.com>
2023-08-15 16:04:10 +0500

    reciept-exampt changes

--- src/api/reciept-exampt.js
+++ src/api/reciept-exampt.js
@@ -6,30 +6,28 @@ const s3 = new AWS.S3();
 export const sendRecieptViaMail = async (req, res) => {
   const { email, recieptContent } = req.body;
   console.log('email: ', email);
-  try {
-    // Generate the PDF receipt from HTML content
-    const pdfBuffer = await generateReceiptPDF(recieptContent);
+  // Generate the PDF receipt from HTML content
+  const pdfBuffer = await generateReceiptPDF(recieptContent);
 
-    // Upload the PDF to S3
-    const fileName = Date.now() + 'Receipt.pdf';
-    const s3Params = {
-      Bucket: 'patronworks', // Replace with your S3 bucket name
-      Key: fileName,
-      Body: pdfBuffer,
-    };
-    await s3.upload(s3Params).promise();
+  // Upload the PDF to S3
+  const fileName = Date.now() + 'Receipt.pdf';
+  const s3Params = {
+    Bucket: 'patronworks', // Replace with your S3 bucket name
+    Key: fileName,
+    Body: pdfBuffer,
+  };
+  await s3.upload(s3Params).promise();
 
-    // Send the receipt PDF as an email attachment
-    const emailSubject = 'Receipt';
-    const attachments = [{ filename: 'Receipt.pdf', path: `https://patronworks.s3.us-west-2.amazonaws.com/${fileName}` }];
+  // Send the receipt PDF as an email attachment
+  const emailSubject = 'Receipt';
+  const attachments = [{ filename: 'Receipt.pdf', path: `https://patronworks.s3.us-west-2.amazonaws.com/${fileName}` }];
 
-    await sendMail(email, emailSubject, recieptContent, attachments);
-    res.status(200).json({ message: 'Receipt sent successfully via email.' });
-  } catch (error) {
-    console.error('Error sending email:', error);
-    res.status(500).json({ error: 'An error occurred while sending the receipt via email.' });
-  }
+  await sendMail(email, emailSubject, recieptContent, attachments).then(res1 => {
+    res.status(200).json({ message: 'Receipt sent successfully via email.'})
+  }).catch(err => {
+    console.error('Error sending email:', err);
 
+  });
 };
 const generateReceiptPDF = async (htmlContent) => {
   const browser = await puppeteer.launch({ headless: 'new' }); // Pass headless: 'new'
