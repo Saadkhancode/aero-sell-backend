f7c21fe49d851a589c698c47b2acf03f1290a6b7
Saadkhancode <saadk6401@gmail.com>
2023-08-15 15:04:27 +0500

    html to pdf file done with aws

--- src/api-routes/reciept-exampt.js
+++ src/api-routes/reciept-exampt.js
@@ -1,8 +1,8 @@
 import express  from "express";
 import {sendRecieptViaMail} from '../api/reciept-exampt.js'
-
+import {awsupload} from "../middlewares/aws-s3-upload.js";
 const router=express.Router()
 
-router.post('/sendReciept',sendRecieptViaMail)
+router.post('/sendReciept',awsupload.single('receipt'),sendRecieptViaMail)
 
 export default router
--- src/api/reciept-exampt.js
+++ src/api/reciept-exampt.js
@@ -1,22 +1,27 @@
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
 import sendMail from '../middlewares/send-email.js';
-import pdf from 'html-pdf'; // Import the html-pdf library
-
+// import pdf from 'html-pdf'; // Import the html-pdf library
+import puppeteer from 'puppeteer';
+import AWS from 'aws-sdk';
+const s3 = new AWS.S3();
 export const sendRecieptViaMail = async (req, res) => {
   const { email, recieptContent } = req.body;
-
-  // Get the current module's URL
-  const currentFilePath = fileURLToPath(import.meta.url);
-
+  console.log('email: ', email);
   try {
     // Generate the PDF receipt from HTML content
     const pdfBuffer = await generateReceiptPDF(recieptContent);
 
+    // Upload the PDF to S3
+    const fileName = Date.now() + 'Receipt.pdf';
+    const s3Params = {
+      Bucket: 'patronworks', // Replace with your S3 bucket name
+      Key: fileName,
+      Body: pdfBuffer,
+    };
+    await s3.upload(s3Params).promise();
+
     // Send the receipt PDF as an email attachment
     const emailSubject = 'Receipt';
-    const attachments = [{ filename: 'Receipt.pdf', content: pdfBuffer }];
+    const attachments = [{ filename: 'Receipt.pdf', path: `https://patronworks.s3.us-west-2.amazonaws.com/${fileName}` }];
 
     await sendMail(email, emailSubject, recieptContent, attachments);
     res.status(200).json({ message: 'Receipt sent successfully via email.' });
@@ -24,22 +29,20 @@ export const sendRecieptViaMail = async (req, res) => {
     console.error('Error sending email:', error);
     res.status(500).json({ error: 'An error occurred while sending the receipt via email.' });
   }
+
 };
-
 const generateReceiptPDF = async (htmlContent) => {
-  return new Promise((resolve, reject) => {
-    // Define options for html-pdf
-    const pdfOptions = {
-      format: 'Letter', // or 'A4', etc.
-    };
+  const browser = await puppeteer.launch({ headless: 'new' }); // Pass headless: 'new'
+  const page = await browser.newPage();
 
-    // Generate PDF from HTML content
-    pdf.create(htmlContent, pdfOptions).toBuffer((err, buffer) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(buffer);
-      }
-    });
-  });
+  // Set the HTML content for the page
+  await page.setContent(htmlContent);
+
+  // Generate the PDF buffer
+  const pdfBuffer = await page.pdf({ format: 'Letter' });
+
+  // Close the browser
+  await browser.close();
+
+  return pdfBuffer;
 };
