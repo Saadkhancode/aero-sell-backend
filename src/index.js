c8fa2eb24da8d7074e537acdf5bebd1cb915f783
Saadkhancode <saadk6401@gmail.com>
2023-08-18 14:14:19 +0500

    pull

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ app.use(cors({
 }));
 
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.json({ limit: '10mb' })); // Example limit of 10MB
+app.use(express.json()); // Example limit of 10MB
 app.use(bodyParser.json())
 
 app.use(helmet({ crossOriginResourcePolicy: false, }));
