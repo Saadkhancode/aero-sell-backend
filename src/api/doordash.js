9927162bffb8060d1b7b9fd984c6a3b2fef98597
Saadkhancode <saadk6401@gmail.com>
2023-08-29 16:28:59 +0500

    dordash api wrok

---
+++ src/api/doordash.js
@@ -0,0 +1,16 @@
+import { DoorDashClient } from "@doordash/sdk";
+import { v4 as uuidv4 } from "uuid";
+
+const client = new DoorDashClient({
+  developer_id: "{your developer_id}",
+  key_id: "{your key_id}",
+  signing_secret: "{your signing_secret}",
+});
+
+const response = await client.createDelivery({
+  external_delivery_id: uuidv4(),
+  pickup_address: "1000 4th Ave, Seattle, WA, 98104",
+  pickup_phone_number: "+1(650)5555555",
+  dropoff_address: "1201 3rd Ave, Seattle, WA, 98101",
+  dropoff_phone_number: "+1(650)5555555",
+});
