418a0f5a73198ded034ed7277a95a0e0c227103f
Ihtizaz Ahmad <ihtizazahmad2002@gmail.com>
2023-08-29 15:24:22 +0500

    new stock added

--- src/models/ingredients.js
+++ src/models/ingredients.js
@@ -1,64 +1,78 @@
-import mongoose from "mongoose"
-const ingredientSchema=new mongoose.Schema({
-    IngredientID:{
-        type:String
+import mongoose from "mongoose";
+
+const stockSchema = new mongoose.Schema({
+    stock: {
+        type: Number,
+        required: true
     },
-    IngredientName:{
-        type:String
+    price: {
+        type: Number,
+        required: true
     },
-    Description:{
-        type:String
+    expiry: {
+        type: String,
+        required: true
     },
-    Expiry:{
-        type:String
-    },
-    UnitofMeasurement:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'mu'
-    },
-    UnitPrice:{
-        type:Number
-    },
-    supplierId:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'Supplier'
-    },
-    CurrentStock:{
-        type:Number
-    },
-    ThresholdLevel:{
-        type:String
-    },
-    Allergens:{
-        type:String
-    },
-    ShelfLife:{
-        type:String
-    },
-    StorageInstructions:{
-        type:String
-    },
-    CategoryType:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'ingredientCategoryModel'
-    },
-    Alternative:{
-        type:String
-    },
-    NutritionalInformation:{
-        type:String
-    },
-    Notes:{
-        type:String
-    },
-    Active:{
-        type:Boolean
-    },
-    userId:{
-        type:mongoose.Schema.Types.ObjectId,
-        required:true,
-        ref:'user'
+    createdAt: {
+        type: Date,
+        default: Date.now
     }
-},{timestamps:true})
-const IngredientsModel=mongoose.model("ingredientsModel",ingredientSchema)
-export default IngredientsModel
+});
+
+const ingredientSchema = new mongoose.Schema({
+    IngredientID: {
+        type: String
+    },
+    IngredientName: {
+        type: String
+    },
+    Description: {
+        type: String
+    },
+    UnitofMeasurement: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'mu'
+    },
+    supplierId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Supplier'
+    },
+    ThresholdLevel: {
+        type: String
+    },
+    Allergens: {
+        type: String
+    },
+    ShelfLife: {
+        type: String
+    },
+    StorageInstructions: {
+        type: String
+    },
+    CategoryType: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'ingredientCategoryModel'
+    },
+    Alternative: {
+        type: String
+    },
+    stockHistory: [stockSchema],
+
+    NutritionalInformation: {
+        type: String
+    },
+    Notes: {
+        type: String
+    },
+    Active: {
+        type: Boolean
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'user'
+    }
+}, { timestamps: true });
+
+const IngredientsModel = mongoose.model("ingredientsModel", ingredientSchema);
+export default IngredientsModel;
