7c0b5dd0c8afd54b970253fbde0eb84d5a62f61e
Saadkhancode <saadk6401@gmail.com>
2023-10-12 14:42:38 +0500

    print api changes

--- src/api-routes/printer-route.js
+++ src/api-routes/printer-route.js
@@ -2,7 +2,25 @@ import express from 'express'
 import {printReceipt} from '../api/print.js'
 import {awsupload} from "../middlewares/aws-s3-upload.js";
  const router=express.Router()
- router.post('/print',awsupload.single('receipt'),printReceipt)
+ router.post('/print', async (req, res) => {
+    try {
+      // Call the printReceipt function and get the PDF content
+      const pdfBuffer = await printReceipt(req, res);
+  
+      // Set the Content-Type header to application/pdf
+      res.setHeader('Content-Type', 'application/pdf');
+  
+      // Set other headers as needed, e.g., Content-Disposition
+      res.setHeader('Content-Disposition', 'attachment; filename="Receipt.pdf"');
+      res.setHeader('Cache-Control', 'no-cache');
+  
+      // Send the PDF content as the response
+      res.send(pdfBuffer);
+    } catch (error) {
+      console.error('Error while processing the request:', error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
 
 
  export default router
