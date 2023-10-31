b0507601ddf2629f8d5b72afcef0691214cbf747
Saadkhancode <saadk6401@gmail.com>
2023-10-31 14:34:52 +0500

    print with ubuntu and window done

--- src/api/print.js
+++ src/api/print.js
@@ -8,35 +8,6 @@ import os from 'os';
 import {exec} from 'child_process'
 const { print } = pkg;
 
-// export const printReceipt = async (req, res) => {
-//   const { content } = req.body;
-//   const __filename = url.fileURLToPath(import.meta.url);
-//   const __dirname = path.dirname(__filename);
-//   const pdfDirectory = path.join(__dirname, 'tmp');
-
-//   if (!fs.existsSync(pdfDirectory)) {
-//     fs.mkdirSync(pdfDirectory, { recursive: true });
-//   }
-
-//   const pdfPath = path.join(pdfDirectory, `${Date.now()}Receipt.pdf`);
-
-//   generateReceiptPDF(content, pdfPath)
-//     .then(() => {
-//       res.setHeader('Content-Type', 'application/pdf');
-//       return print(pdfPath);
-//     })
-//     .then(() => {
-//       console.log('Print done');
-//       res.status(200).send('Print successful');
-//     })
-//     .catch(error => {
-//       console.log('Error while printing:', error);
-//       res.status(400).send(`Error while printing: ${error}`);
-//     })
-//     .finally(() => {
-//       fs.unlinkSync(pdfPath);
-//     });
-// };
 
 
 export const printReceipt = async (req, res) => {
